5dba11bff49189e591d6d1e5ffb2831f6767c917
Olivier Revollat <revollat@gmail.com>
2018-10-04 16:21:55 +0200

    tests

--- cp_test2.py
+++ cp_test2.py
@@ -13,58 +13,58 @@ c = Configuration()
 c.assert_hostname = False
 Configuration.set_default(c)
 api = core_v1_api.CoreV1Api()
-name = 'busybox-test'
+name = 'nginx-deploymentmonblog-6cc4595cb7-llqv5'
 
-resp = None
-try:
-    resp = api.read_namespaced_pod(name=name,
-                                   namespace='default')
-except ApiException as e:
-    if e.status != 404:
-        print("Unknown error: %s" % e)
-        exit(1)
-
-if not resp:
-    print("Pod %s does not exits. Creating it..." % name)
-    pod_manifest = {
-        'apiVersion': 'v1',
-        'kind': 'Pod',
-        'metadata': {
-            'name': name
-        },
-        'spec': {
-            'containers': [{
-                'image': 'busybox',
-                'name': 'sleep',
-                "args": [
-                    "/bin/sh",
-                    "-c",
-                    "while true;do date;sleep 5; done"
-                ]
-            }]
-        }
-    }
-    resp = api.create_namespaced_pod(body=pod_manifest,
-                                     namespace='default')
-    while True:
-        resp = api.read_namespaced_pod(name=name,
-                                       namespace='default')
-        if resp.status.phase != 'Pending':
-            break
-        time.sleep(1)
-    print("Done.")
+# resp = None
+# try:
+#     resp = api.read_namespaced_pod(name=name,
+#                                    namespace='default')
+# except ApiException as e:
+#     if e.status != 404:
+#         print("Unknown error: %s" % e)
+#         exit(1)
+#
+# if not resp:
+#     print("Pod %s does not exits. Creating it..." % name)
+#     pod_manifest = {
+#         'apiVersion': 'v1',
+#         'kind': 'Pod',
+#         'metadata': {
+#             'name': name
+#         },
+#         'spec': {
+#             'containers': [{
+#                 'image': 'busybox',
+#                 'name': 'sleep',
+#                 "args": [
+#                     "/bin/sh",
+#                     "-c",
+#                     "while true;do date;sleep 5; done"
+#                 ]
+#             }]
+#         }
+#     }
+#     resp = api.create_namespaced_pod(body=pod_manifest,
+#                                      namespace='default')
+#     while True:
+#         resp = api.read_namespaced_pod(name=name,
+#                                        namespace='default')
+#         if resp.status.phase != 'Pending':
+#             break
+#         time.sleep(1)
+#     print("Done.")
 
 # Calling exec interactively.
 
 exec_command = ['sh']
 resp = stream(api.connect_get_namespaced_pod_exec, name, 'default',
-			  command=exec_command,
-			  stderr=True, stdin=True,
-			  stdout=True, tty=False,
-			  _preload_content=False)
+              command=exec_command,
+              stderr=True, stdin=True,
+              stdout=True, tty=False,
+              _preload_content=False)
 
 source_file = '/tmp/dash'
-destination_file = '/tmp/dest'
+destination_file = '/usr/share/nginx/html/index.html'
 file = open(source_file, "r")
 
 commands = []
@@ -73,49 +73,16 @@ commands.append(file.read())
 commands.append("EOF\n")
 
 while resp.is_open():
-	resp.update(timeout=1)
-	if resp.peek_stdout():
-		print("STDOUT: %s" % resp.read_stdout())
-	if resp.peek_stderr():
-		print("STDERR: %s" % resp.read_stderr())
+    resp.update(timeout=1)
+    if resp.peek_stdout():
+        print("STDOUT: %s" % resp.read_stdout())
+    if resp.peek_stderr():
+        print("STDERR: %s" % resp.read_stderr())
 
-	if commands:
-		c = commands.pop(0)
-		resp.write_stdin(c)
-	else:
-		break
+    if commands:
+        c = commands.pop(0)
+        resp.write_stdin(c)
+    else:
+        break
 
 resp.close()
-	
-# Copying file client -> pod
-#print('copying client -> pod')
-#exec_command = ['tar', 'xvf', '-', '-C', '/']
-#resp = stream(api.connect_get_namespaced_pod_exec, name, 'default',
-#              command=exec_command,
-#              stderr=True, stdin=True,
-#              stdout=True, tty=False,
-#              binary=True,
-#              _preload_content=False)
-#
-#source_file = '/tmp/dash'
-#
-#with TemporaryFile() as tar_buffer:
-#    with tarfile.open(fileobj=tar_buffer, mode='w') as tar:
-#        tar.add(source_file)
-#
-#    tar_buffer.seek(0)
-#    commands = []
-#    commands.append(tar_buffer.read())
-#
-#    while resp.is_open():
-#        resp.update(timeout=1)
-#        if resp.peek_stdout():
-#            print("STDOUT: %s" % resp.read_stdout())
-#        if resp.peek_stderr():
-#            print("STDERR: %s" % resp.read_stderr())
-#        if commands:
-#            c = commands.pop(0)
-#            resp.write_stdin(c)
-#        else:
-#            break
-#    resp.close()
--- kublog.py
+++ kublog.py
@@ -1,17 +1,17 @@
 from kubernetes import client, config, watch
+from kubernetes.client.apis import core_v1_api
+from kubernetes.stream import stream
 import os
 import hashlib
 
-from kubernetes.client.rest import ApiException
-from pprint import pprint
+# from kubernetes.client.rest import ApiException
+# from pprint import pprint
 
 DOMAIN = "revollat.net"
 DEPLOYMENT_NAME = "nginx-deployment"
 
-def process(crds, obj, operation):
 
-    metadata = obj.get("metadata")
-    name = metadata.get("name")
+def process(crds, obj, operation, metadata, name):
     namespace = metadata.get("namespace")
     content = obj["spec"]["content"]
     hash_object = hashlib.sha256(content.encode('utf-8'))
@@ -25,25 +25,25 @@ def process(crds, obj, operation):
     service.api_version = "v1"
     service.kind = "Service"
     service.metadata = client.V1ObjectMeta(name="my-service-" + name)
-    spec = client.V1ServiceSpec()
-    spec.selector = {"app": "nginx" + name}
-    spec.ports = [client.V1ServicePort(protocol="TCP", port=80, target_port=9376)]
-    service.spec = spec
+    svcspec = client.V1ServiceSpec()
+    svcspec.selector = {"app": "nginx" + name}
+    svcspec.ports = [client.V1ServicePort(protocol="TCP", port=80, target_port=9376)]
+    service.spec = svcspec
 
     if operation == "ADDED":
+
         create_deployment(extensions_v1beta1, deployment)
 
-
         api_instance.create_namespaced_service(namespace="default", body=service)
-
 
         obj["spec"]["contenthash"] = hex_dig
         crds.replace_namespaced_custom_object(DOMAIN, "v1", namespace, "kublogs", name, obj)
 
     elif operation == "MODIFIED":
+
         contenthash = obj["spec"]["contenthash"]
 
-        if hex_dig == contenthash :
+        if hex_dig == contenthash:
             print("Pas de changement ...")
         else:
             print("Une modif a eu lieu ...")
@@ -51,16 +51,46 @@ def process(crds, obj, operation):
             crds.replace_namespaced_custom_object(DOMAIN, "v1", namespace, "kublogs", name, obj)
 
             ret = api_instance.list_namespaced_pod(namespace="default", label_selector="app=nginx"+name)
-            print(ret.items[0].metadata.name)
+            podname = ret.items[0].metadata.name
+
+            print("DEBUG : podname = " + podname)
+
+            exec_command = ['sh']
+
+            api = core_v1_api.CoreV1Api()
+
+            resp = stream(api.connect_get_namespaced_pod_exec, podname, 'default', command=exec_command,
+                          stderr=True, stdin=True, stdout=True, tty=False, _preload_content=False)
+
+            source_file = '/tmp/thetest'
+            destination_file = '/usr/share/nginx/html/index.html'
+            file = open(source_file, "r")
+            commands = list()
+            commands.append("cat <<'EOF' >" + destination_file + "\n")
+            commands.append(file.read())
+            commands.append("EOF\n")
+
+            while resp.is_open():
+                resp.update(timeout=1)
+                if resp.peek_stdout():
+                    print("STDOUT: %s" % resp.read_stdout())
+                if resp.peek_stderr():
+                    print("STDERR: %s" % resp.read_stderr())
+                if commands:
+                    c = commands.pop(0)
+                    resp.write_stdin(c)
+                else:
+                    break
+
+            resp.close()
 
     elif operation == "DELETED":
-        delete_deployment(extensions_v1beta1, name)
 
+        delete_deployment(extensions_v1beta1, name)
         api_instance.delete_namespaced_service(name="my-service-"+name, namespace="default", body=service)
 
 
-
-def create_deployment_object(name):
+def create_deployment_object(deployname):
     # Configureate Pod template container
     container = client.V1Container(
         name="nginx",
@@ -68,7 +98,7 @@ def create_deployment_object(name):
         ports=[client.V1ContainerPort(container_port=80)])
     # Create and configurate a spec section
     template = client.V1PodTemplateSpec(
-        metadata=client.V1ObjectMeta(labels={"app": "nginx"+name}),
+        metadata=client.V1ObjectMeta(labels={"app": "nginx"+deployname}),
         spec=client.V1PodSpec(containers=[container]))
     # Create the specification of deployment
     spec = client.ExtensionsV1beta1DeploymentSpec(
@@ -78,7 +108,7 @@ def create_deployment_object(name):
     deployment = client.ExtensionsV1beta1Deployment(
         api_version="extensions/v1beta1",
         kind="Deployment",
-        metadata=client.V1ObjectMeta(name=DEPLOYMENT_NAME+name),
+        metadata=client.V1ObjectMeta(name=DEPLOYMENT_NAME+deployname),
         spec=spec)
 
     return deployment
@@ -103,29 +133,61 @@ def delete_deployment(api_instance, name):
     print("Deployment deleted. status='%s'" % str(api_response.status))
 
 
-
 if __name__ == "__main__":
     if 'KUBERNETES_PORT' in os.environ:
         config.load_incluster_config()
     else:
         config.load_kube_config()
-		
+
     configuration = client.Configuration()
     configuration.assert_hostname = False
     api_client = client.api_client.ApiClient(configuration=configuration)
     crds = client.CustomObjectsApi(api_client)
 
-    #try: 
-        #api_instance = client.CoreV1Api()
-        #api_response = api_instance.list_namespaced_pod(namespace="default", watch=False)
-        #pprint(api_response)	
-    #except ApiException as e:
+    # try:
+    #     api_instance = client.CoreV1Api()
+    #     api_response = api_instance.list_namespaced_pod(namespace="default", watch=False)
+    #     pprint(api_response)
+    # except ApiException as e:
     #   print("Exception when calling CoreV1Api->list_namespaced_pod: %s\n" % e)
-	
+
+    # # --------------------------------
+    # exec_command = ['sh']
+    # api = core_v1_api.CoreV1Api()
+    #
+    # resp = stream(api.connect_get_namespaced_pod_exec, "nginx-deploymentmonblog-6cc4595cb7-kdhvk",
+    #               'default', command=exec_command, stderr=True, stdin=True, stdout=True, tty=False, _preload_content=False)
+    #
+    # source_file = '/tmp/thetest'
+    # destination_file = '/usr/share/nginx/html/index.html'
+    # file = open(source_file, "r")
+    # commands = list()
+    # commands.append("cat <<'EOF' >" + destination_file + "\n")
+    # commands.append(file.read())
+    # commands.append("EOF\n")
+    #
+    # while resp.is_open():
+    #     resp.update(timeout=1)
+    #     if resp.peek_stdout():
+    #         print("STDOUT: %s" % resp.read_stdout())
+    #     if resp.peek_stderr():
+    #         print("STDERR: %s" % resp.read_stderr())
+    #     if commands:
+    #         c = commands.pop(0)
+    #         resp.write_stdin(c)
+    #     else:
+    #         break
+    #
+    # resp.close()
+    #
+    # exit(0)
+    # # --------------------------------
+
     print("Watch for kublogs ...")
     resource_version = ''
     while True:
-        stream = watch.Watch().stream(crds.list_cluster_custom_object, DOMAIN, "v1", "kublogs", resource_version=resource_version)
+        stream = watch.Watch().stream(crds.list_cluster_custom_object, DOMAIN, "v1", "kublogs",
+                                      resource_version=resource_version)
         for event in stream:
             obj = event["object"]
             operation = event['type']
@@ -136,4 +198,4 @@ if __name__ == "__main__":
             resource_version = metadata['resourceVersion']
             name = metadata['name']
             print("Handling %s on %s" % (operation, name))
-            process(crds, obj, operation)
+            process(crds, obj, operation, metadata, name)
